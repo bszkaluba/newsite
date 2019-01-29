d0f3764c093a88c72b11cc9a272cf04f02051681
bszkaluba <47151692+bszkaluba@users.noreply.github.com>
2019-01-29 16:08:48 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+var burgerbutton=document.getElementById("burgerbutton");
+burgerbutton.addEventListener("click", toggleNav, false);
+
+
+
+function toggleNav () {
+	var bar1=document.getElementById("bar1")
+	var bar2=document.getElementById("bar2")
+	var bar3=document.getElementById("bar3")
+	var navbar=document.getElementById("navbar")
+	
+	
+	if(navbar.className === "mob-nav") {
+		bar1.classList.add("rotate-bar1");
+		bar2.classList.add("hidebar2");
+		bar3.classList.add("rotate-bar3");
+		navbar.classList.add("show-nav");
+	} else {
+		bar1.classList.remove("rotate-bar1");
+		bar2.classList.remove("hidebar2");
+		bar3.classList.remove("rotate-bar3");
+		navbar.classList.remove("show-nav");
+	}
+	
+}
+
